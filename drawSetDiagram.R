c9f1f1aeb783f1ee7021b7780de6767f42b1d9ff
supersayan <sayandutta135@gmail.com>
2021-08-11 22:45:06 -0400

    3set intrsct and exampl Rmd

--- drawSetDiagram.R
+++ drawSetDiagram.R
@@ -46,9 +46,13 @@ drawSet <- function(plot, x, y, r, fill, name, elements, elementXOffset=0, eleme
   step <- r/6
   x0 <- x
   y0 <- y
-  d <- plot + geom_circle(aes(x0 = x0, y0 = y0, r = r), fill="red", color="NA", alpha=0.4, size=0)
+  d <- plot + geom_circle(aes(x0 = x0, y0 = y0, r = r), fill=fill, color="NA", alpha=0.4, size=0)
   x <- x+elementXOffset
   y <- y+elementYOffset
+  
+  # x,y are offset values, x0,y0 are the original
+  
+  # if/else for adding elements based on number, up to 5
   if (length(elements) == 0) {
   } else if (length(elements) == 1) {
     d <- d + geom_text(aes(x=x-step, y=y+step, label=elements[1]), size=8) +
@@ -87,20 +91,22 @@ drawSet <- function(plot, x, y, r, fill, name, elements, elementXOffset=0, eleme
       geom_point(aes(x=x+step, y=y-step*1.5), size=4)
   }
   
+  # allow different positions for the set name
   if (namePosition == 1 | namePosition == "tr") {
     # top right
-    d <- d + geom_text(aes(x=x+step*4, y=y+step*6, label=name), size=8)
+    d <- d + geom_text(aes(x=x0+step*4, y=y0+step*6, label=name), size=8)
   } else if (namePosition == 2 | namePosition == "bl") {
     # bot left
-    d <- d + geom_text(aes(x=x-step*4, y=y-step*6, label=name), size=8)
+    d <- d + geom_text(aes(x=x0-step*4, y=y0-step*6, label=name), size=8)
   } else if (namePosition == 3 | namePosition == "br") {
     # bot right
-    d <- d + geom_text(aes(x=x+step*4, y=y-step*6, label=name), size=8)
+    d <- d + geom_text(aes(x=x0+step*4, y=y0-step*6, label=name), size=8)
   } else {
     # top left : default
-    d <- d + geom_text(aes(x=x-step*4, y=y+step*6, label=name), size=8)
+    d <- d + geom_text(aes(x=x0-step*4, y=y0+step*6, label=name), size=8)
   }
   
+  #return new plot
   d
 }
 
@@ -140,6 +146,8 @@ drawSet2 <- function(plot, x, y, r, set1Name, set1Elements, set2Name, set2Elemen
 drawSet3 <- function(plot, x, y, r, set1Name, set1Elements, set2Name, set2Elements, set3Name, set3Elements) {
   d <- plot
   step <- r/6
+  # font size for element labels in intersecting regions
+  intrsctfontsize <- 7
   
   setIntersection123 <- intersect(intersect(set1Elements, set2Elements), set3Elements)
   setIntersection12 <- setdiff(intersect(set1Elements, set2Elements), setIntersection123)
@@ -160,59 +168,119 @@ drawSet3 <- function(plot, x, y, r, set1Name, set1Elements, set2Name, set2Elemen
       # set3
       d <- drawSet(d, 15, 10, 7, "yellow", set3Name, set3Elements, elementXOffset = 0, elementYOffset = -2*step, namePosition = "br")
       # 23
-      if (length(setIntersection23) > 0) {
-        d <- d + geom_text(aes(x=1, y=15, label=setIntersection23[1]), size=6) +
-          geom_point(aes(x=18, y=14), size=4)
-      }
+      # if (length(setIntersection23) > 0) {
+      #   d <- d + geom_text(aes(x=1, y=15, label=setIntersection23[1]), size=intrsctfontsize) +
+      #     geom_point(aes(x=18, y=14), size=4)
+      #   d <- drawIntersectionElements(d, setIntersection13, 11, 16, -0.3)
+      # }
+      d <- drawIntersectionElements(d, setIntersection23, 18.5, 13.5, -0.2)
       # 12
-      if (length(setIntersection12) > 0) {
-        d <- d + geom_text(aes(x=14, y=20, label=setIntersection12[1]), size=6) +
-          geom_point(aes(x=15, y=19), size=4)
-      }
+      # if (length(setIntersection12) > 0) {
+      #   d <- d + geom_text(aes(x=14, y=20, label=setIntersection12[1]), size=intrsctfontsize) +
+      #     geom_point(aes(x=15, y=19), size=4)
+      # }
+      d <- drawIntersectionElements(d, setIntersection12, 15, 19, 0.5)
       # 13
-      if (length(setIntersection13) > 0) {
-        d <- d + geom_text(aes(x=11, y=15, label=setIntersection13[1]), size=6) +
-          geom_point(aes(x=12, y=14), size=4)
-      }
+      # if (length(setIntersection13) > 0) {
+      #   d <- d + geom_text(aes(x=11, y=15, label=setIntersection13[1]), size=intrsctfontsize) +
+      #     geom_point(aes(x=12, y=14), size=4)
+      # }
+      d <- drawIntersectionElements(d, setIntersection13, 11.5, 13.5, 0.2)
       # 123
       if (length(setIntersection123) > 0) {
-        d <- d + geom_text(aes(x=14, y=16.5, label=setIntersection123[1]), size=6) +
+        d <- d + geom_text(aes(x=14, y=16.5, label=setIntersection123[1]), size=intrsctfontsize) +
           geom_point(aes(x=15, y=15.5), size=4)
       }
   } else if (length(setIntersection12) == 0 & length(setIntersection13) == 0 & length(setIntersection23) == 0) {
     # no intersecting
-    d <- drawSet(d, 8, 20, r, "red", set1Name, set1Elements, namePosition = "tr")
+    d <- drawSet(d, 8, 20, r, "red", set1Name, set1Elements, namePosition = "tl")
     d <- drawSet(d, 22, 20, r, "green", set2Name, set2Elements, namePosition = "tr")
     d <- drawSet(d, 15, 8, r, "yellow", set3Name, set3Elements, namePosition = "br")
   } else {
-    Intersect2 <- FALSE
     if (length(setIntersection12) == 0) {
       if (length(setIntersection13) == 0) {
         # 23
+        d <- drawSet(d, 8, 20, r, "red", set1Name, set1Elements, namePosition = "tl")
+        d <- drawSet(d, 21, 17, r, "green", set2Name, set2Elements, elementXOffset = 1, elementYOffset = 2, namePosition = "tr")
+        d <- drawSet(d, 17, 10, r, "yellow", set3Name, set3Elements, elementXOffset = -1, elementYOffset = -2, namePosition = "br")
+        d <- drawIntersectionElements(d, setIntersection23, 19, 13.5, -0.2)
       } else {
         if (length(setIntersection23) == 0) {
           # 13
+          d <- drawSet(d, 9, 17, r, "red", set1Name, set1Elements, elementXOffset = -1, elementYOffset = 2, namePosition = "tl")
+          d <- drawSet(d, 22, 20, r, "green", set2Name, set2Elements, namePosition = "tr")
+          d <- drawSet(d, 13, 10, r, "yellow", set3Name, set3Elements, elementXOffset = 1, elementYOffset = -2, namePosition = "br")
+          d <- drawIntersectionElements(d, setIntersection13, 11, 13.5, 0.2)
         } else {
           # 13 and 23
-          Intersect2 <- TRUE
+          d <- drawSet(d, 7, 14, r, "red", set1Name, set1Elements, elementXOffset = -1, namePosition = "tl")
+          d <- drawSet(d, 23, 14, r, "green", set2Name, set2Elements, elementXOffset = 1, namePosition = "tr")
+          d <- drawSet(d, 15, 18, r, "yellow", set3Name, set3Elements, elementYOffset = 2, namePosition = "tr")
+          d <- drawIntersectionElements(d, setIntersection13, 11, 16, -0.3)
+          d <- drawIntersectionElements(d, setIntersection23, 19, 16, 0.3)
         }
       }
     } else if (length(setIntersection13) == 0) {
       if (length(setIntersection23) == 0) {
         # 12
+        d <- drawSet(d, 11, 20, r, "red", set1Name, set1Elements, elementXOffset = -2, namePosition = "tl")
+        d <- drawSet(d, 19, 20, r, "green", set2Name, set2Elements, elementXOffset = 2, namePosition = "tr")
+        d <- drawSet(d, 15, 8, r, "yellow", set3Name, set3Elements, namePosition = "br")
+        d <- drawIntersectionElements(d, setIntersection12, 15, 20, 0.5)
       } else {
         # 12 and 23
-        Intersect2 <- TRUE
+        d <- drawSet(d, 7, 14, r, "red", set1Name, set1Elements, elementXOffset = -1, namePosition = "tl")
+        d <- drawSet(d, 15, 18, r, "green", set2Name, set2Elements, elementYOffset = 2, namePosition = "tr")
+        d <- drawSet(d, 23, 14, r, "yellow", set3Name, set3Elements, elementXOffset = 1, namePosition = "tr")
+        d <- drawIntersectionElements(d, setIntersection12, 11, 16, -0.3)
+        d <- drawIntersectionElements(d, setIntersection23, 19, 16, 0.3)
       }
     } else {
       # 12 and 13
-      Intersect2 <- TRUE
+      d <- drawSet(d, 15, 18, r, "red", set1Name, set1Elements, elementYOffset = 2, namePosition = "tr")
+      d <- drawSet(d, 7, 14, r, "green", set2Name, set2Elements, elementXOffset = -1, namePosition = "tl")
+      d <- drawSet(d, 23, 14, r, "yellow", set3Name, set3Elements, elementXOffset = 1, namePosition = "tr")
+      d <- drawIntersectionElements(d, setIntersection12, 11, 16, -0.3)
+      d <- drawIntersectionElements(d, setIntersection13, 19, 16, 0.3)
     }
   }
   
   d
 }
 
-drawSetDiagram('G', c('a', '2', 'c'))
-drawSetDiagram('A', c('a', '2', 'c'), 'B', c('a', '2', 'b'))
-drawSetDiagram('A', c('a', '2', 'c'), 'B', c('a', '2', 'b'), 'D', c('a', '3', 'f'))
+drawIntersectionElements <- function(plot, elements, x, y, angle, fontsize=7) {
+  # angle is in half-rotations (radians / pi)
+  d <- plot
+  # font size for element labels in intersecting regions
+  
+  # assumes <0 means angle between 0,-0.5 or 0.5,1 to prevent dot overlapping label
+  xstep <- -1
+  if (angle < 0) {
+    xstep <- 1
+  }
+  
+  # adds up to 2 elements in the given intersection. if 2, draws in a line at the specified angle
+  if (length(elements) == 0) {
+  } else if (length(elements) == 1) {
+    d <- d + geom_text(aes(x=x-1, y=y+1, label=elements[1]), size=fontsize) +
+      geom_point(aes(x=x, y=y), size=4)
+  } else {
+    d <- d + geom_text(aes(x=x+cospi(angle)+xstep, y=y+sinpi(angle)+1, label=elements[1]), size=fontsize) +
+      geom_point(aes(x=x+cospi(angle), y=y+sinpi(angle)), size=4) +
+      geom_text(aes(x=x-cospi(angle)+xstep, y=y-sinpi(angle)+1, label=elements[2]), size=fontsize) +
+      geom_point(aes(x=x-cospi(angle), y=y-sinpi(angle)), size=4)
+  }
+  
+  d
+}
+
+# drawSetDiagram('G', c('a', '2', 'c'))
+# drawSetDiagram('A', c('a', '2', 'c'), 'B', c('a', '2', 'b'))
+# drawSetDiagram('A', c('c'), 'B', c('b'))
+# drawSetDiagram('A', c('a', '2', 'c'), 'B', c('a', '2', 'b'), 'D', c('a', '3', 'f', 'h', 'u', 'i', 'o'))
+# drawSetDiagram('A', c('a', '2', 'c'), 'B', c('b'), 'D', c('3', 'f', 'h', 'u', 'i', 'o'))
+# 
+# drawSetDiagram('A', c('1', '2', '3', '0'), 'B', c('4', '5', '6'), 'C', c('0', '7', '8', '9'))
+# drawSetDiagram('A', c('1', '2', '3'), 'B', c('4', '5', '6', '0'), 'C', c('0', '8', '9'))
+# drawSetDiagram('A', c('1', '2', '3', '0'), 'B', c('4', '5', '6', '0'), 'C', c('7', '8', '9'))
+# drawSetDiagram('A', c('1', '2', '3', '0', 'c', 'd', 'e'), 'B', c('4', '5', '6', '0', 'a', 'b'), 'C', c('7', '8', '9', 'a', 'b', 'e'))
